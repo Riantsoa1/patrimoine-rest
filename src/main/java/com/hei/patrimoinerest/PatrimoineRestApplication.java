48247cf83a3d2b3ac00d1b6c853b10a07bd0e70a
= <=>
2024-09-23 14:44:02 +0300

    chore: init patrimoine-rest

---
+++ src/main/java/com/hei/patrimoinerest/PatrimoineRestApplication.java
@@ -0,0 +1,13 @@
+package com.hei.patrimoinerest;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PatrimoineRestApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(PatrimoineRestApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/hei/patrimoinerest/PatrimoineRestApplicationTests.java
@@ -0,0 +1,13 @@
+package com.hei.patrimoinerest;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PatrimoineRestApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
